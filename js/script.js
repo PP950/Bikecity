4d70efdf7967e3da7e21b8f6106f8cb7b351ca73
Paulo <paulopocas20@gmail.com>
2024-05-01 18:08:12 -0300

    adicionando slideshow

--- js/script.js
+++ js/script.js
@@ -16,3 +16,20 @@
      // Atualiza o índice para a próxima cor de fundo na próxima chamada
      currentColorIndex = (currentColorIndex + 1) % backgroundColors.length;
  }
+ 
+ //SlideShow
+ let imagens = ['img/produto1.jpg', 'img/produto2.jpg', 'img/produto3.jpg', 'img/produto4.jpg'];
+let index = 0;
+let time = 3000;
+
+function slideShow() {
+    document.getElementById('imgBanner').src = imagens[index];
+    index++;
+
+    if (index == imagens.length) {
+        index = 0;
+    }
+    setTimeout(slideShow, time); // Chama a função slideShow diretamente
+}
+slideShow(); // Chama a função slideShow para iniciar o slideshow
+
