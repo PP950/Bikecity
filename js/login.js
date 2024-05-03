2720dd9a18f61918d7f3e1639fdb769d14f9660a
Paulo <paulopocas20@gmail.com>
2024-05-03 20:00:05 -0300

    corrigindo erros

--- js/login.js
+++ js/login.js
@@ -19,3 +19,21 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         alert('Credenciais inválidas. Tente novamente.');
     }
 });
+//Lista de cores de fundo
+const backgroundColors = ['#FFFFFF', '#000000', '#ffffcc'];
+let currentColorIndex = 0;
+
+//Função para mudar a cor de fundo
+function changeBackgroundColor() {
+    //Seleciona o elemento body
+    const body = document.body;
+
+    //Obtém a próxima cor de fundo da lista
+    const nextColor = backgroundColors[currentColorIndex];
+
+    //Define a nova cor de fundo
+    body.style.backgroundColor = nextColor;
+
+    //Atualiza o body para a próxima cor de fundo na próxima chamada
+    currentColorIndex = (currentColorIndex + 1) % backgroundColors.length;
+}
