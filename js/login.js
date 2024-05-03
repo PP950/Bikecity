bd967a491ac067f27618cb4846e212800d38f02f
Paulo <paulopocas20@gmail.com>
2024-05-03 19:45:19 -0300

    Documentando codigos

--- js/login.js
+++ js/login.js
@@ -1,19 +1,19 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário
+    event.preventDefault(); //Evita o envio do formulário
 
-    // Captura dos valores dos campos de entrada
+    //Captura dos valores dos campos de entrada
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    // Verifica se os campos estão preenchidos
+    //Verifica se os campos estão preenchidos
     if (username.trim() === '' || password.trim() === '') {
         alert('Por favor, preencha todos os campos.');
         return;
     }
 
-    // Verifica as credenciais
+    //Verifica as credenciais
     if (username === 'usuario' && password === 'senha') {
-        // Redireciona para a página do quiz
+        //Redireciona para a página do quiz
         window.location.href = 'quiz.html';
     } else {
         alert('Credenciais inválidas. Tente novamente.');
